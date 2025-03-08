a8f702a254e7e50e3084c351a06632dc447b44a2
yuxuanleong <97377660+yuxuanleong@users.noreply.github.com>
2025-03-08 22:51:08 +0800

    Add basic unit test for Register page

--- client/src/pages/Auth/Register.test.js
+++ client/src/pages/Auth/Register.test.js
@@ -48,6 +48,25 @@ describe('Register Component', () => {
     jest.clearAllMocks();
   });
 
+  it('renders registration form', () => {
+    const { getByText, getByPlaceholderText } = render(
+      <MemoryRouter initialEntries={['/register']}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(getByText('REGISTER FORM')).toBeInTheDocument();
+    expect(getByPlaceholderText('Enter Your Name')).toBeInTheDocument();
+    expect(getByPlaceholderText('Enter Your Email')).toBeInTheDocument();
+    expect(getByPlaceholderText('Enter Your Password')).toBeInTheDocument();
+    expect(getByPlaceholderText('Enter Your Phone')).toBeInTheDocument();
+    expect(getByPlaceholderText('Enter Your Address')).toBeInTheDocument();
+    expect(getByPlaceholderText('Enter Your DOB')).toBeInTheDocument();
+    expect(getByPlaceholderText('What is Your Favorite sports')).toBeInTheDocument();
+  });
+
   it('should register the user successfully', async () => {
     axios.post.mockResolvedValueOnce({ data: { success: true } });
 
