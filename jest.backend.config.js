322e2e5cc4f943a060701f24c0b64aed14d3db9f
yuxuanleong <97377660+yuxuanleong@users.noreply.github.com>
2025-03-09 16:48:21 +0800

    Include middlewares folder in test folders

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -8,13 +8,15 @@ export default {
   // which test to run
   testMatch: [
     "<rootDir>/controllers/*.test.js", 
-    "<rootDir>/helpers/*.test.js"],
+    "<rootDir>/helpers/*.test.js",
+    "<rootDir>/middlewares/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
     "controllers/**", 
-    "helpers/**"],
+    "helpers/**",
+    "middlewares/**"],
   coverageThreshold: {
     global: {
       lines: 100,
