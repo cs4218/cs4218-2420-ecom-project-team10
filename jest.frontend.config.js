48dc09741043302907192396c7b327f745a4df32
Yingming23 <liyingming13@gmail.com>
2025-03-10 00:33:25 +0800

    Updated package.json

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,4 +1,5 @@
 export default {
+  setupFiles: ["<rootDir>/jest.setup.js"],
   // name displayed during tests
   displayName: "frontend",
 
@@ -20,7 +21,14 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/*.test.js", "<rootDir>/client/src/pages/Auth/*.test.js", "<rootDir>/client/src/components/Form/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/components/Form/*.test.js",
+    "<rootDir>/client/src/pages/*.test.js",
+    "<rootDir>/client/src/pages/admin/*.test.js",
+    "<rootDir>/client/src/context/*.test.js",
+    "<rootDir>/client/src/components/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
